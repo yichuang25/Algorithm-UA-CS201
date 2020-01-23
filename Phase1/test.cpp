3598f1e1c66a9d049889acc791a265b33911ecb4
yhuang87 <yhuang87@crimson.ua.edu>
2020-01-23 16:27:54 -0600

    update test file

--- Phase1/test.cpp
+++ Phase1/test.cpp
@@ -4,8 +4,16 @@
 using namespace std;
 
 int main () {
-    CDA<double> a;
-
+    CDA<int> a(1);
+    cout << a.Capacity() << endl;
+    a.AddEnd(1);
+    a.Printarray();
+    a.AddEnd(2);
+    a.Printarray();
+    a.AddEnd(3);
+    a.AddEnd(4);
+    a.AddEnd(5);
+    a.Printarray();
 
     return 0;
 }
