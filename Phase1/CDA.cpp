744c3e45bda58a3ca102ac516971e3055e34f8bd
yhuang87 <yhuang87@crimson.ua.edu>
2020-01-26 20:17:49 -0600

    Update method

--- Phase1/CDA.cpp
+++ Phase1/CDA.cpp
@@ -17,7 +17,10 @@ public:
     CDA();
     CDA(int s);
     ~CDA();
+    CDA(const CDA &cda);
+    CDA& operator=(const CDA &src);
     elmtype& operator[](int i);
+    void Printarray();
     void AddEnd(elmtype v);
     void AddFront(elmtype v);
     void DelEnd();
@@ -27,9 +30,9 @@ public:
     int Clear();
     bool Ordered();
     int SetOrdered();
+    void QuickSort();
     elmtype Select(int k);
     void InsertionSort();
-    void QuickSort();
     void CountingSort(int m);
     int Search(elmtype e);
 };
@@ -63,54 +66,250 @@ CDA<elmtype> :: ~CDA() {
      order = false;
 }
 
+
+//Copy constructor
+template <class elmtype>
+CDA<elmtype> :: CDA(const CDA &src){
+    cout << "In copy Constructor\n";
+    capacity = src.capacity;
+    end = src.end;
+    front = src.front;
+    length = src.length;
+    order = src.order;
+    array = new elmtype[capacity];
+    for(int i=0;i<length;i++) {
+        array[(front+i)%capacity] = src.array[(front+i)%capacity];
+    }
+}
+
+//Copy assignment operator
+template <class elmtype>
+CDA<elmtype>& CDA<elmtype> :: operator=(const CDA<elmtype> &src) {
+    cout << "In copy assignment operator\n";
+    if (this != src) {
+        capacity = src.capacity;
+        end = src.end;
+        length = src.length;
+        order = src.order;
+        array = new elmtype[capacity];
+        for(int i=0;i<length;i++) {
+            array[(front+i)%capacity] = src.array[(front+i)%capacity];
+        }
+    }
+    return *this;
+} 
+
 template <class elmtype>
 elmtype& CDA<elmtype> :: operator[](int i) {
     
 }
 
 template <class elmtype>
-void CDA<elmtype> :: AddEnd(elmtype v) {
+void CDA<elmtype> :: Printarray() {
+    cout << "Capacity: " << capacity << endl;
+    cout << "Length: " << length << endl;
+    cout << "Front: " << front << endl;
+    cout << "End: " << end << endl;
+    cout << "Array\n";
+    for (int i=0; i<length;i++){
+            cout << array[(front + i)%capacity] << " ";
+        }
+    cout << endl << endl;
+}
 
+template <class elmtype>
+void CDA<elmtype> :: AddEnd(elmtype v) {
+    if (length == capacity){ // Out of space
+        capacity = 2 * capacity;
+        cout << "Doubling to: " << capacity << endl;
+        elmtype *temp = new elmtype[capacity];
+        for (int i=0; i<length;i++){
+            temp[i] = array[(front + i)%length];
+        }
+        delete array;
+        array = new elmtype[capacity];
+        for(int i=0;i<length;i++){
+            array[i] = temp[i];
+        }
+        free(temp);
+        front = 0;
+        end = length-1;
+    }
+    if (length == 0) { //no element
+        array[front] = v;
+        length++;
+    }
+    else {
+        array[end + 1] = v;
+        end++;
+        length++;
+    }
+
+    
 }
 
 template <class elmtype>
 void CDA<elmtype> :: AddFront(elmtype v) {
-
+    if (length == capacity){ // Out of space
+        capacity = 2 * capacity;
+        cout << "Doubling to: " << capacity << endl;
+        elmtype *temp = new elmtype[capacity];
+        for (int i=0; i<length;i++){
+            temp[i] = array[(front + i)%length];
+        }
+        free (array);
+        array = new elmtype[capacity];
+        for(int i=0;i<length;i++){
+            array[i] = temp[i];
+        }
+        free(temp);
+        front = 0;
+        end = length-1;
+    }
+    if (length == 0) { //no element
+        array[front] = v;
+        length++;
+    }
+    else {
+        if (front == 0) {
+            front = capacity -1;
+            array[front] = v;
+            length++;
+        }
+        else {
+            front--;
+            array[front] = v;
+            length++;
+        }
+    }
 }
 
 template <class elmtype>
 void CDA <elmtype> :: DelEnd(){
-
+    if(length == 0) {
+        cout << "The array is empty\n";
+        front = 0;
+        end = 0;
+    }
+    
+    else {
+        if(end == 0) {
+            end = capacity - 1;
+            length--;
+        }
+        else {
+            end--;
+            length--;
+        }
+    
+        if(length <= capacity * 0.25){
+            capacity = capacity/2;
+            cout << "Reducing to : " << capacity << endl;
+            elmtype *temp = new elmtype[capacity];
+            for (int i=0; i<length;i++){
+                temp[i] = array[(front + i)%(2*capacity)];
+            }
+            free(array);
+            array = new elmtype[capacity];
+            for(int i=0;i<length;i++){
+                array[i] = temp[i];
+            }
+            front = 0;
+            end = length-1;
+        }
+        if(length == 0) {
+            end = 0;
+            front =0;
+        }
+    }
+        
 }
 
 template <class elmtype>
 void CDA <elmtype> :: DelFront() {
+    if (length == 0){
+        cout << "THe array is empty\n";
+        front = 0;
+        end = 0;
+    }
+    else {
+        front = (front + 1) % capacity;
+        length--;
 
+        if(length <= capacity * 0.25){
+            capacity = capacity/2;
+            cout << "Reducing to : " << capacity << endl;
+            elmtype *temp = new elmtype[capacity];
+            for (int i=0; i<length;i++){
+                temp[i] = array[(front + i)%(2*capacity)];
+            }
+            free(array);
+            array = new elmtype[capacity];
+            for(int i=0;i<length;i++){
+                array[i] = temp[i];
+            }
+            front = 0;
+            end = length-1;
+        }
+        if (length == 0) {
+            front = 0;
+            end = 0;
+        }
+    }
 }
 
 template <class elmtype>
 int CDA <elmtype> :: Length() {
-
+    return length;
 }
 
 template <class elmtype>
 int CDA <elmtype> :: Capacity() {
-
+    return capacity;
 }
 
 template <class elmtype>
 int CDA <elmtype> :: Clear(){
+    capacity = 1;
+    length = 0;
+    order = false;
+    front = 0;
+    end = 0;
+    free(array);
+    array = new elmtype[1];
 
 }
 
 template <class elmtype> 
 bool CDA <elmtype> :: Ordered(){
-
+    return order;
 }
 
 template <class elmtype>
 int CDA <elmtype> :: SetOrdered() {
+    if (length == 0) {
+        cout << "The array is empty\n";
+        order = false;
+        return -1;
+    }
+    order = true;
+    for(int i=0;i<length-1;i++) {
+        if(array[(front + i) % capacity] > array[(front + i + 1)%capacity]){
+            order = false;
+        }
+    }
 
+    if (order == false){
+        return -1;
+    }
+    else {
+        return 1;
+    }
+}
+
+template  <class elmtype>
+void CDA <elmtype> :: QuickSort() {
+    
 }
 
 template <class elmtype>
@@ -120,12 +319,13 @@ elmtype CDA <elmtype> :: Select(int k){
 
 template <class elmtype>
 void CDA <elmtype> :: InsertionSort() {
-
-}
-
-template  <class elmtype>
-void CDA <elmtype> :: QuickSort() {
-
+    for(int i=1;i<length;i++) {
+        for(int j=i-1;j>=0 && array[(front+j+1)%capacity] < array[(front+j)%capacity];j--){
+            elmtype temp = array[(front+j)%capacity];
+            array[(front+j)%capacity] = array[(front+j+1)%capacity];
+            array[(front+j+1)%capacity] = temp;
+        }
+    }
 }
 
 template <class elmtype>
--- Phase1/test.cpp
+++ Phase1/test.cpp
@@ -4,16 +4,99 @@
 using namespace std;
 
 int main () {
-    CDA<int> a(1);
-    cout << a.Capacity() << endl;
+    /*cout << "Test add end\n";
+    CDA<int> a;
+    a.Printarray();
+
+    a.AddEnd(0);
+    a.Printarray();
+
     a.AddEnd(1);
     a.Printarray();
+
     a.AddEnd(2);
     a.Printarray();
+
     a.AddEnd(3);
-    a.AddEnd(4);
-    a.AddEnd(5);
     a.Printarray();
+
+    a.AddEnd(4);
+    a.Printarray();
+
+    a.AddEnd(5);
+    a.Printarray();*/
+    cout << "Test add Front\n";
+    CDA<int> b;
+    b.Printarray();
+
+    b.AddFront(0);
+    b.Printarray();
+
+    b.AddFront(1);
+    b.Printarray();
+
+    b.AddFront(2);
+    b.Printarray();
+
+    b.AddFront(3);
+    b.Printarray();
+
+    b.AddFront(4);
+    b.Printarray();
+
+    b.AddFront(5);
+    b.Printarray();
+
+    b.AddEnd(6);
+    b.Printarray();
+
+    b.AddEnd(7);
+    b.Printarray();
+
+    b.AddFront(8);
+    b.Printarray();
+
+    b.AddEnd(4);
+    b.Printarray();
+
+    b.SetOrdered();
+    cout << b.Ordered() << endl;
+
+    b.InsertionSort();
+    b.Printarray();
+    b.SetOrdered();
+    cout << b.Ordered() << endl;
+
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
+
+    b.DelFront();
+    b.Printarray();
 
     return 0;
 }
